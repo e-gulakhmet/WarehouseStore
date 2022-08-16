2ae877b1bed3f20b1fb0482dd084f5c435d1f805
who-man-tech <enes515254@gmail.com>
2022-08-16 14:53:07 +0200

    Added fixtures for Warehouse and Store accounts

--- app/main/settings/common.py
+++ app/main/settings/common.py
@@ -96,3 +96,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+FIXTURE_DIRS = ['fixtures']
